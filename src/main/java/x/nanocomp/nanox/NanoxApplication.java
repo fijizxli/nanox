1805d6e3ac082132dff1ea4c2984f5acf608d48f
Szabó Zoltán <szabozoltan6x@gmail.com>
2023-08-06 18:07:21 +0200

    Initial commit.

---
+++ src/main/java/x/nanocomp/nanox/NanoxApplication.java
@@ -0,0 +1,13 @@
+package x.nanocomp.nanox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class NanoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NanoxApplication.class, args);
+    }
+}
